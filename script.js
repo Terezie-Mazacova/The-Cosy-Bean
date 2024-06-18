02a52d408ae8f1f778209f9d944f66629aaea705
Terezie Mazáčová <terezkamazacova@post.cz>
2024-06-18 14:55:24 +0200

    rozdelana struktura sekce Section Filter menu

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     var splide1 = new Splide( '.slider1', {
+        perPage: 2,
+        gap: '5px',
+        pagination: false,
+        type: 'loop',
+        autoplay: true,
+        interval: 1000,
+        speed: 900000,
+        arrows: false
+    });
+    splide1.mount();
+
+    var splide2 = new Splide( '.slider2', {
         perPage: 4,
         gap: '20px',
         pagination: false,
         type: 'loop',
         autoplay: true,
         interval: 1000,
-        speed: 90000,
+        speed: 900000,
         arrows: false
     });
-    splide1.mount();
+    splide2.mount();
 } );
